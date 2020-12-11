e1f0279d345c1a9f55535ecf59ae95e84dcdfe51
davron1989 <davron.n@yahoo.com>
2020-12-11 20:56:51 +0000

    merged config role and sns files inot one

--- mian.tf
+++ mian.tf
@@ -66,7 +66,7 @@ resource "aws_config_config_rule" "s3-bucket-server-side-encryption-enabled" {
 }
 
 resource "aws_s3_bucket" "my-config" {
-  bucket = "config-bucket-for-my-test-project3"
+  bucket = "config-bucket-for-my-test-project4"
   acl    = "private"
 
   versioning {
@@ -77,3 +77,41 @@ resource "aws_s3_bucket" "my-config" {
     prevent_destroy = true
   }
 }
+
+
+###################
+##SNS TOPIC BLOCK##
+###################
+
+## Locals
+
+locals {
+  default_tags = {
+    "Terraform"        = "true"
+    "Terraform-Module" = "deanwilson-sns-email"
+  }
+}
+
+data "template_file" "cloudformation_sns_stack" {
+  template = file("${path.module}/templates/email-sns-stack.json.tpl")
+
+  vars = {
+    display_name  = var.display_name
+    email_address = var.email_address
+    protocol      = var.protocol
+  }
+}
+
+resource "aws_cloudformation_stack" "sns-topic" {
+  name          = var.stack_name
+  template_body = data.template_file.cloudformation_sns_stack.rendered
+
+  tags = merge(
+    local.default_tags,
+    var.additional_tags,
+    {
+      "Name" = var.stack_name
+    },
+  )
+}
+
